a9ecd2b6e6c7091be5bc4cd502d2d8eb6677c781
Yan P Aung <yan.pye.aung@gmail.com>
2020-08-30 10:30:38 -0700

    Set default view of the App component by calling search on componentDidMount

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
 
+    componentDidMount() {
+        this.onTermSubmit('yosemite valley');
+    }
+
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
             params: {
