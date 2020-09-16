5f200aedfcee5590a7fca66c9dc78c738f277c9a
Yan P Aung <yan.pye.aung@gmail.com>
2020-09-16 06:55:24 -0700

    Refactor fetching of vidoes as custom hook and use that instead in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
-import youtube from '../apis/youtube';
 import VideoDetail from './VideoDetail';
+import useVideos from '../hooks/useVideos';
 
 
 const App = () => {
-    const [videos, setVideos] = useState([]);
     const [selectedVideo, setSelectedVideo] = useState(null);
+    const [videos, search] = useVideos('Yosemite Valley');
 
     useEffect(() => {
-        onTermSubmit('Yosemite Valley');
-    }, []);
-
-    const onTermSubmit = async term => {
-        const response = await youtube.get('/search', {
-            params: {
-                'q': term
-            }
-        });
-
-        setVideos(response.data.items);
         // Select the first video automatically by default
-        setSelectedVideo(response.data.items[0]);
-    };
+        setSelectedVideo(videos[0]);
+    }, [videos]);
 
     return (
         <div className="ui container">
-            <SearchBar onFormSubmit={onTermSubmit} />
+            <SearchBar onFormSubmit={search} />
             <div className="ui grid">
                 <div className="ui row">
                     <div className="eleven wide column">
